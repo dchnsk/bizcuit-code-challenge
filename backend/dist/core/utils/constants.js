cf275f5f18a00fe782e2b4d10afff07a4f6d4439
Valerii Dychanskyi <107610663+dchnsk@users.noreply.github.com>
2023-02-16 22:06:40 +0100

    Delete backend/dist directory

--- backend/dist/core/utils/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CRYPTO_KEY = exports.CRYPTO_ALGORITHM = exports.REQ_BODY_SIZE_LIMIT = exports.DEFAULT_PORT = void 0;
-exports.DEFAULT_PORT = 5000;
-exports.REQ_BODY_SIZE_LIMIT = "10kb";
-// TODO: Should be moved to .env
-exports.CRYPTO_ALGORITHM = "aes256";
-exports.CRYPTO_KEY = "vdchnsk";
--- backend/dist/interfaces/todo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TodoStatus = void 0;
-var TodoStatus;
-(function (TodoStatus) {
-    TodoStatus["uncompleted"] = "uncompleted";
-    TodoStatus["completed"] = "completed";
-})(TodoStatus = exports.TodoStatus || (exports.TodoStatus = {}));
